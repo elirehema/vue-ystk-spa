d9965e6d0383d67f9668c5b61deb8ebd2c934ac9
=elirehema <=rocksandystone@gmail.com>
2020-01-12 18:23:38 +0300

    Added Index Page

--- pages/men/index.vue
+++ pages/men/index.vue
@@ -1,5 +1,13 @@
 <template>
-    <v-app class="app">
-        <v-container fluid></v-container>
-    </v-app>
+  <v-app class="app">
+    <v-container grid-list-xl text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs10 offset-xs1>
+          <v-card dark color="primary">
+            <v-card-text>MEN</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
