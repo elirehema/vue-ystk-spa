1b059c60e4403b13d317f2d15ef753e84cb87395
=elirehema <=rocksandystone@gmail.com>
2020-01-12 18:17:39 +0300

    large text

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,10 @@
             </v-flex>
             <v-flex grow xs12 sm6 md7 order-md2 order-sm2>
               <v-card dark tile flat color="rgb(255, 0, 0, 0.1)">
-                <v-card-text class="black--text">{{ description }}</v-card-text>
+                <v-card-text class="black--text">
+                  <span v-if="$vuetify.breakpoint.smAndDown">{{ description }}</span>
+                  <span v-if="$vuetify.breakpoint.mdAndUp">{{ description_large }}</span>
+                </v-card-text>
               </v-card>
             </v-flex>
           </v-layout>
@@ -58,6 +61,8 @@ export default {
         "health, white, grey, turquoise, fresh, clean, feminine," +
         "minimal, web design, website design, professional, brand design, brand identity," +
         "website design inspiration, feminine branding design",
+      description_large:
+        "Magic Hands Massage — spa website design inspiration, spawebsite design ideas, spa website layout, spa, beauty,health, white, grey, turquoise, fresh, clean, feminine,minimal, web design, website design, professional, brand design, brand identity,website design inspiration, feminine branding designIf you’re quite active on Instagram, you probably know just howcommon inspirational quotes are on the platform. The truth is, they work.They never cease to get engagement. But this doesn’t just happen on Instagram:people simply love relating to positive statements! That’s also why many salonsand spas these days will have light boxes sitting at the front desk, displayinga fun or inspiring saying. So whether you’re looking for social media material,a bank of spa quotes to insert at the bottom of amarketing email or display on your light box, we’ve got you covered! Magic Hands Massage — spa website design inspiration, spawebsite design ideas, spa website layout, spa, beauty,health, white, grey, turquoise, fresh, clean, feminine,minimal, web design, website design, professional, brand design, brand identity,website design inspiration, feminine branding designIf you’re quite active on Instagram, you probably know just howcommon inspirational quotes are on the platform. The truth is, they work.They never cease to get engagement. But this doesn’t just happen on Instagram:people simply love relating to positive statements! That’s also why many salonsand spas these days will have light boxes sitting at the front desk, displayinga fun or inspiring saying. So whether you’re looking for social media material,a bank of spa quotes to insert at the bottom of amarketing email or display on your light box, we’ve got you covered!",
       title: "Magic Hands Massage"
     };
   }
