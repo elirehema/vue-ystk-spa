0b7b55a47155f63012249ea5d9cc55d5132c200e
=elirehema <=rocksandystone@gmail.com>
2020-01-14 19:50:00 +0300

    Added Men and Women Update

--- pages/men/index.vue
+++ pages/men/index.vue
@@ -1,13 +1,42 @@
 <template>
-  <v-app class="app">
-    <v-container grid-list-xl text-xs-center>
-      <v-layout row wrap>
-        <v-flex xs10 offset-xs1>
-          <v-card dark color="primary">
-            <v-card-text>MEN</v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-app>
+  <gender-component :data="infos"></gender-component>
 </template>
+<script>
+import GenderComponent from "~/components/women/GenderComponent.vue";
+export default {
+  components: {
+    GenderComponent
+  },
+  data() {
+    return {
+      infos: {
+        title: "",
+        subtitle: "SiTAMET CONSTECTETUR ADIPISCI VELIT",
+        background: "https://www.apexdspa.in/assets/img/slider/main-xs-3.jpg",
+        image: "http://femaletomalebodymassageinmumbai.com/img/second.jpg",
+        description:
+          "Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet." +
+          "Mauris cursus commodo interdum. Praesent ut risus eget metus luctus" +
+          "accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim" +
+          "a sit amet dui." +
+          "Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet." +
+          "Mauris cursus commodo interdum. Praesent ut risus eget metus luctus" +
+          "accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim" +
+          "a sit amet dui." +
+          "Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet." +
+          "Mauris cursus commodo interdum. Praesent ut risus eget metus luctus" +
+          "accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim" +
+          "a sit amet dui." +
+          "Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet." +
+          "Mauris cursus commodo interdum. Praesent ut risus eget metus luctus" +
+          "accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim" +
+          "a sit amet dui." +
+          "Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet." +
+          "Mauris cursus commodo interdum. Praesent ut risus eget metus luctus" +
+          "accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim" +
+          "a sit amet dui."
+      }
+    };
+  }
+};
+</script>
